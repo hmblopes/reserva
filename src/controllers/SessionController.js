9391055cc2224edf5abf9ba9fd01e7fa0e84e841
hmblopes <beatrizsantoslopes26@gmail.com>
2024-05-13 08:47:03 -0300

    Configurando a pasta Models

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,16 @@
+/*
+index(listAll): listagem de sessões
+store(add): criar uma sessão
+show(list): Quando queremos listar uma unica sessão
+update: alterar alguma sessão
+destroy(delete): Quando queremos deletar uma sessão
+*/
+
+import User from '../models/User'
+import * as Yup from 'yup'
+
+class SessionController {
+    async store(req,res) {
+        
+    }
+}
