8f5f24796caa0a816a1671f2dcf3b5f41c667ac4
hmblopes <beatrizsantoslopes26@gmail.com>
2024-05-20 08:51:56 -0300

    Conectando banco

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import multer from 'multer'
 import uploadConfig from './config/upload'
-import SessionController from './controllers/SessionControllers'
+import SessionController from './controllers/SessionController'
 
 const routes = new Router() 
 
--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 import app from './app'
 
-app.lister(4000)
+app.listen(4000)
