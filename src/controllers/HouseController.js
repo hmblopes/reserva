04c59cd393fef909151261b5421a3dd4f6d5da52
hmblopes <beatrizsantoslopes26@gmail.com>
2024-06-03 08:48:45 -0300

    Update do HouseController

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -32,6 +32,35 @@ class HouseController {
         })
         return res.json(house)
     }
+    async update(req, res) {
+        const schema = Yup.object().shape({
+            description: Yup.string().required(),
+            price: Yup.number().required(),
+            location: Yup.string().required(),
+            status: Yup.boolean().required(),
+        })
+        const { filename } = req.file
+        const { house_id } = req.params
+        const { description, price, location, status } = req.body
+        const { user_id } = req.headers
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Falha na validação.' })
+        }
+        const user = await User.findById(user_id)
+        const houses = await House.findById(house_id)
+        if (String(user._id) !== String(houses.user)){
+            return res.status(401).json({ error: 'Não autorizado' })
+        }
+        await House.updateOne({_id: house_id }, {
+            user: user_id,
+            thumbnail: filename,
+            description,
+            price,
+            location,
+            status,
+        })
+        return res.send()
+    }
 }
 
 export default new HouseController()
