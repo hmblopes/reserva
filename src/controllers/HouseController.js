2027482dc6b829b29326fc6276fcac980276166b
hmblopes <beatrizsantoslopes26@gmail.com>
2024-06-10 08:25:06 -0300

    Criando HouseController em pasta controllers

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -61,6 +61,18 @@ class HouseController {
         })
         return res.send()
     }
+
+    async destroy(req, res) {
+        const { house_id } = req.body
+        const { user_id } = req.headers
+        const user = await User.findById(user_id)
+        const houses = await House.findById(house_id)
+        if (String(user._id) !== String(houses.user)) {
+            return res.status(401).json({ error: 'Não autorizado' })
+        }
+        await House.findByIdAndDelete({ _id: house_id })
+        return res.json({ message: 'Excluído com sucesso!' })
+    }
 }
 
 export default new HouseController()
